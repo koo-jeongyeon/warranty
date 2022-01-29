ca181373ba8ea5d48a6f9d12faacd43de09603ca
KIMBEOMJUNG <rla5451@naver.com>
2022-01-30 01:51:30 +0900

    axios 통신 추가, test컴포넌트 추가

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    testtest
+    <test msg="test App"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    test,
   }
 }
 </script>
---
+++ src/frontend/src/components/test.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    test 입니다. test컴포넌트
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+    console.log("testeeeee");
+    this.testF();
+    axios.get('/api/hello').then(res => { console.log(res.data) });//백엔드 통신
+  },
+  methods: {
+    testF(){
+      console.log("test");
+      return 
+    },
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
